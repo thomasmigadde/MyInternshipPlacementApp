816b69f8868cff5a907fc35eff417149ded395f1
Thomas1 <thomasmigadde@gmail.com>
2026-04-14 19:15:41 +0300

    commiter

--- end/internMonitor/placements/views.py
+++ end/internMonitor/placements/views.py
@@ -1,9 +1,38 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.decorators import api_view
 from .models import StakeHolder
 from .serializers import StakeHolderSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
-class StakeHolderViewSet(viewsets.ModelViewSet):
-    queryset = StakeHolder.objects.all()
-    serializer_class = StakeHolderSerializer
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+
+def StakeHolder_list(request):
+    if request.method == 'GET':
+        stakeholders = StakeHolder.objects.all()
+        serializer = StakeHolderSerializer(stakeholders, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StakeHolderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'PUT':
+        try:
+            stakeholder = StakeHolder.objects.get(pk=request.data.get('id'))
+        except StakeHolder.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = StakeHolderSerializer(stakeholder, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        try:
+            stakeholder = StakeHolder.objects.get(pk=request.data.get('id'))
+        except StakeHolder.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        stakeholder.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
