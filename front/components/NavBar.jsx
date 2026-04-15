e3820c390a222cdb4d05e44d28153e0a02ba036a
Thomas1 <thomasmigadde@gmail.com>
2026-04-15 09:23:24 +0300

    add

--- front/components/NavBar.jsx
+++ front/components/NavBar.jsx
@@ -6,9 +6,6 @@ function NavBar() {
         <div>
           <Link to="/">Home</Link>
           <Link to="/SignupPage">Signup</Link>
-          <Link to="/SignupPage2">Signup</Link>
-          <Link to="/SignupPage3">Signup</Link>
-          <Link to="/SignupPage4">Signup</Link>
         </div>
         </nav>
     );
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,15 @@
 
 import Home from "../PAGES/home.jsx"
-import "../STYLES/loginPage.css"
-import "../STYLES/SignupPage.css"
-import SignupPage from "../PAGES/signuppage.jsx"
-import SignupPage2 from "../PAGES/signuppage2.jsx"
-import SignupPage3 from "../PAGES/signuppage3.jsx"
-import SignupPage4 from "../PAGES/signuppage4.jsx"
+import "/STYLES/loginPage.css"
+import "/STYLES/SignupPage.css"
+import SignupPage from "/PAGES/signuppage.jsx"
+import SignupPage2 from "/PAGES/signuppage2.jsx"
+import SignupPage3 from "/PAGES/signuppage3.jsx"
+import SignupPage4 from "/PAGES/signuppage4.jsx"
 import {Routes, Route} from 'react-router-dom'
 import NavBar from '../components/NavBar';
-import "../STYLES/SignupPage.css"
+import "/STYLES/SignupPage.css"
+import "/STYLES/loginPage.css"
 
 function App() {
   return(
@@ -20,9 +21,7 @@ function App() {
         <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/SignupPage" element={<SignupPage />} />
-              <Route path="/SignupPage2" element={<SignupPage2 />} />
-              <Route path="/SignupPage3" element={<SignupPage3 />} />
-              <Route path="/SignupPage4" element={<SignupPage4 />} />
+              <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>  
     </div>
