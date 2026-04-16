705d06406e017347b65e0e4f671574b9d080119b
Thomas1 <thomasmigadde@gmail.com>
2026-04-16 11:49:23 +0300

    techguy

--- front/PAGES/DisplayPage.jsx
+++ front/PAGES/DisplayPage.jsx
@@ -1,14 +1,39 @@
 
 import React from "react";
-
+import {useState, useEffect} from "react";
 import { useNavigation } from "./usenavigation.jsx";
+import {api} from "front/api.js"
 
-export default function DisplayPage() {
-  
+export default function DisplayPage({userId}) {
+const [user , setUser] = useState(null)
+
+useEffect (() => {
+try{
+  const fetchUser = async () => {
+    const response = await api.get(`/stakeholders/${userId}`);
+    setUser(data);
+  }
+}  catch(error){
+  console.error("Error fetching user data:", error)} ;
+  setError("Error fetching user data. Please try again.");
+});  
+
+if (userId) {
+  fetchUser();
+}
+[userId]
+
+
   return (
+  <>
+    {user ? (
        <div className="signup-page-container">
-       <p>The page
-        </p>     
-    </div>
+       <h1>{user.username}</h1> 
+       <h1>{user.email}</h1>    
+    </div>) : (<>
+      <p>User not found.</p>
+    </>)
+    }
+  </>
   );
 }
