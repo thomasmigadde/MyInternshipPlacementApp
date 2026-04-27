58017aa33114185c177a64b93a2d14a386a5de41
Thomas1 <thomasmigadde@gmail.com>
2026-04-27 11:18:09 +0300

    new

--- end/internMonitor/placements/views.py
+++ end/internMonitor/placements/views.py
@@ -3,10 +3,10 @@ from rest_framework.decorators import api_view
 from .models import StakeHolder
 from .serializers import StakeHolderSerializer
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+@api_view(['GET', 'POST'])
 
 def StakeHolder_list(request):
     if request.method == 'GET':
@@ -19,20 +19,18 @@ def StakeHolder_list(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'PUT':
+    
+@api_view(['GET', 'PUT', 'DELETE'])    
+def Stakeholder_detail(request, pk):    
+    
         try:
-            stakeholder = StakeHolder.objects.get(pk=request.data.get('id'))
+            stakeholder = StakeHolder.objects.get(pk=pk)
         except StakeHolder.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
         serializer = StakeHolderSerializer(stakeholder, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
+        
+        if request.method == 'GET':
+            serializer = StakeHolderSerializer(stakeholder)
             return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        try:
-            stakeholder = StakeHolder.objects.get(pk=request.data.get('id'))
-        except StakeHolder.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        stakeholder.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        elif request.method == 'PUT':
+             serializer = StakeHolderSerializer(stakeholder, data=request.data)
