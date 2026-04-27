085b037843f73fe011a5ca61e73e1ef668b23477
Thomas1 <thomasmigadde@gmail.com>
2026-04-27 11:21:56 +0300

    urls.py

--- end/internMonitor/placements/urls.py
+++ end/internMonitor/placements/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     
      #path('', include(router.urls)),
      path('stakeholders/', StakeHolder_list, name='stakeholder-list'),
+     path('stakeholders/<int:pk>/', views.Stakeholder_detail, name='stakeholder-detail'),
      ]
